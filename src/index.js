2275d663fb369778f2c942ac8161a5bcc703dce5
Chris Boette <cboette@gmail.com>
2017-03-12 15:24:34 -0500

    Start of loading in Clippy into component.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-export default React.createClass({
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+import './clippy.css';
+import clippy from './clippy.js';
+
+class ReactClippy extends React.Component {
+  constructor (props) {
+    super(props);
+    console.log('props in constructor', props);
   }
-})
+
+  componentDidMount () {
+    const { actor, animation, dialog } = this.props;
+    clippy.load(actor, (agent) => {
+      agent.show();
+      animation && agent.play(animation);
+      dialog && agent.speak(dialog);
+    });
+  }
+
+  render () {
+    return (<div>
+      <h2>Welcome to React components</h2>
+    </div>);
+  }
+}
+
+ReactClippy.propTypes = {
+  actor: React.PropTypes.string.isRequired,
+  animation: React.PropTypes.string,
+  dialog: React.PropTypes.string
+};
+
+export default ReactClippy;
